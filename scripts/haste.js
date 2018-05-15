99552dc4c55491314f2af18173e962b5e181c66b
Jon <jjsang13@gmail.com>
2018-05-15 16:49:10 -0400

    Finished Haste personas

--- scripts/haste.js
+++ scripts/haste.js
@@ -1,34 +1,67 @@
 
-    var ctx = $("#myChart");
+    var ctx = $("#myChart1");
     
     var myChart = new Chart(ctx, {
         type: 'doughnut',
         data: {
-            labels: ["Red", "Blue", "Yellow"],
+            labels: ["Yes", "A little", "No"],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3],
+                label: 'Are you Tech Savvy?',
+                data: [66.7, 22.2, 11.1],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)'
+                    'rgba(255, 0, 0, 0.7)',
+                    'rgba(255, 179, 26, 0.9)',
+                    'rgba(5, 76, 255, 0.9)'
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)'
+                    'rgba(255, 5, 5, 0)',
+                    'rgba(255, 5, 5, 0)',
+                    'rgba(255, 5, 5, 0)'
                 ],
                 borderWidth: 1
             }]
         },
         options: {
             cutoutPercentage: 50,
-            scales: {
                 yAxes: [{
                     ticks: {
                         beginAtZero: true
                     }
                 }]
-            }
-        }
+         }
     });
+
+    var ctx = $("#myChart2");
+    
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["0-1 hour", "2 hours", "3+ hours"],
+            datasets: [{
+                label: 'How long do you spend shopping at the supermarket?',
+                data: [81.5, 18.5, 0],
+                backgroundColor: [
+                    'rgba(255, 0, 0, 0.7)',
+                    'rgba(255, 179, 26, 0.9)',
+                    'rgba(5, 76, 255, 0.9)'
+                ],
+                borderColor: [
+                    'rgba(255, 5, 5, 0)',
+                    'rgba(255, 5, 5, 0)',
+                    'rgba(255, 5, 5, 0)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            cutoutPercentage: 50,
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+         }
+    });
+
+
+    
